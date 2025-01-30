59b05b0167adeb1950532851fc784d8b3ffa5d20
KoranitAnanpoonsap <koranit.bm@gmail.com>
2025-01-30 20:37:24 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/financial-planner/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
